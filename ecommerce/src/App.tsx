41190379a5f90d9e92df5d423a0f7b2b402de33e
FadyFathey <fadyfathymaher3@gmail.com>
2025-12-05 17:39:29 +0200

    add back to home btn

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,15 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/home/HomePage'
+import Signup from './pages/auth/Signup'
+import Login from './pages/auth/Login'
+import ForgotPassword from './pages/auth/ForgotPassword'
 
 function App() {
-
   return (
-    <>
-      <div className=''>
-         <HomePage />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
